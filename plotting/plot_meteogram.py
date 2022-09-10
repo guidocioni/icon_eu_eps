947d44580b2c5d1aee8e973f5ebbe939e549f746
Guido Cioni <guidocioni@gmail.com>
2022-09-10 18:15:35 +0200

    Added precipitation probability to meteogram

--- plotting/plot_meteogram.py
+++ plotting/plot_meteogram.py
@@ -1,12 +1,13 @@
 import time
 from tqdm.contrib.concurrent import process_map
 import sys
-from utils import get_city_coordinates, read_dataset, processes, folder_images
+from utils import get_city_coordinates, read_dataset, processes, folder_images, convert_timezone
 import matplotlib.dates as mdates
 import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import gridspec
+import pandas as pd
 import matplotlib
 matplotlib.use('Agg')
 
@@ -52,14 +53,18 @@ def main():
 def plot(dset_city):
     city = dset_city.attrs['city']
     print('Producing meteogram for %s' % city)
-    dset_city['t2m'] = dset_city['t2m'].metpy.convert_units('degC')
+    dset_city['t2m'] = dset_city['t2m'].metpy.convert_units('degC').metpy.dequantify()
+    dset_city['valid_time'] = convert_timezone(
+        pd.to_datetime(dset_city.valid_time))
 
     nrows = 3
     ncols = 1
     sns.set(style="white")
 
     locator = mdates.AutoDateLocator(minticks=12, maxticks=36)
-    formatter = mdates.ConciseDateFormatter(locator, show_offset=False)
+    formatter = mdates.ConciseDateFormatter(locator,
+                                            show_offset=False,
+                                            formats=['%y', '%b', '%d %b', '%H EST', '%H EST', '%S'])
 
     fig = plt.figure(1, figsize=(9, 10))
     gs = gridspec.GridSpec(nrows, ncols, height_ratios=[1, 1, 1])
@@ -67,9 +72,9 @@ def plot(dset_city):
     ax_temp = plt.subplot(gs[0])
     ax_temp.set_title("ICON-EU-EPS meteogram for "+city+" | Run " +
                       dset_city.time.dt.strftime('%Y%m%d %H UTC').item())
-    ax_temp.plot(dset_city.valid_time,
+    ax_temp.plot(dset_city['valid_time'],
                  dset_city['t2m'].values.T, '-', linewidth=0.8, zorder=1)
-    ax_temp.set_xlim(dset_city.valid_time[0], dset_city.valid_time[-1])
+    ax_temp.set_xlim(dset_city['valid_time'][0], dset_city['valid_time'][-1])
     ax_temp.xaxis.set_major_locator(locator)
     ax_temp.xaxis.set_major_formatter(formatter)
     ax_temp.yaxis.grid(True)
@@ -81,16 +86,34 @@ def plot(dset_city):
 
     ax_prec = plt.subplot(gs[1])
     for member in range(1, len(dset_city.number)):
-        ax_prec.bar(dset_city.rain_rate.sel(number=member).valid_time,
+        ax_prec.bar(dset_city['valid_time'],
                     dset_city.rain_rate.sel(number=member).values, width=0.035,
                     alpha=0.2, color='blue', zorder=1)
-        ax_prec.bar(dset_city.snow_rate.sel(number=member).valid_time,
+        ax_prec.bar(dset_city['valid_time'],
                     dset_city.snow_rate.sel(number=member).values, width=0.035,
                     alpha=0.2, color='purple', zorder=2)
+
+    # Add text on top of the bars
+    y_max = dset_city['rain_rate'].max(dim='number')
+    x = dset_city['valid_time']
+    prob = ((dset_city['rain_rate'] > 0.1).sum(
+            dim='number') / len(dset_city.number)) * 100
+    for i, _ in enumerate(prob):
+        if prob[i] > 0:
+            ax_prec.annotate(
+                ("%d%%" % prob[i]),
+                (x[i], y_max[i]),
+                color='black',
+                weight='bold',
+                fontsize=6,
+                zorder=5,
+                horizontalalignment='center',
+                rotation=90)
+
     ax_prec.yaxis.grid(True)
     ax_prec.xaxis.grid(True, color='gray', linewidth=0.2)
     ax_prec.set_ylim(bottom=0)
-    ax_prec.set_xlim(dset_city.valid_time[0], dset_city.valid_time[-1])
+    ax_prec.set_xlim(dset_city['valid_time'][0], dset_city['valid_time'][-1])
     ax_prec.xaxis.set_major_locator(locator)
     ax_prec.xaxis.set_major_formatter(formatter)
     ax_prec.tick_params(axis='y', which='major', labelsize=8)
@@ -99,20 +122,20 @@ def plot(dset_city):
     ax_prec.set_ylabel("Rain (blue) and snow (purple)", fontsize=8)
 
     ax_clouds = plt.subplot(gs[2])
-    ax_clouds.plot(dset_city.valid_time,
+    ax_clouds.plot(dset_city['valid_time'],
                    dset_city['CLCT'].values.T, 'o', zorder=1)
-    ax_clouds.plot(dset_city.valid_time, dset_city['CLCT'].mean(dim='number'), '-',
+    ax_clouds.plot(dset_city['valid_time'], dset_city['CLCT'].mean(dim='number'), '-',
                    linewidth=2, zorder=2, color='black')
     ax_clouds.yaxis.grid(True)
     ax_clouds.xaxis.grid(True, color='gray', linewidth=0.2)
     ax_clouds.set_ylim(bottom=0)
-    ax_clouds.set_xlim(dset_city.valid_time[0], dset_city.valid_time[-1])
+    ax_clouds.set_xlim(dset_city['valid_time'][0], dset_city['valid_time'][-1])
     ax_clouds.xaxis.set_major_locator(locator)
     ax_clouds.xaxis.set_major_formatter(formatter)
     for label in ax_clouds.get_xticklabels(which='major'):
         label.set(rotation=45)
     ax_clouds.tick_params(axis='y', which='major', labelsize=8)
-    ax_clouds.tick_params(axis='x', which='both', bottom=False)
+    ax_clouds.tick_params(axis='x', which='both', labelsize=8)
     ax_clouds.set_ylabel("Cloud cover", fontsize=8)
 
     fig.subplots_adjust(hspace=0.05)
